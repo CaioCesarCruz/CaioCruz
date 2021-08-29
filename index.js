d3bc50fe375dc21cfe50e48672e6f917a7339c87
Caio Cruz <caio.cesar.amorim.cruz@gmail.com>
2021-08-29 20:09:18 -0300

    implementação da gravação de restaurantes

--- index.js
+++ index.js
@@ -2,13 +2,33 @@ var express = require('express');
 var app = express();
 app.use(express.json());
 
+var restaurantes = [
+    {
+        "codigo": 1,
+        "nome": "Spoletto"
+    },
+    {
+        "codigo": 2,
+        "nome": "Podrão do Seu Zé"
+    },
+    {
+        "codigo": 3,
+        "nome": "China in Box"
+    }];
+
 app.get('/api/helloworld', function(req, res) {
     console.log(req.body);
     res.send('Hello World');
 });
 
+app.get('/api/retornatodososrestaurantes', function(req, res) {
+    console.log(req.body);
+    res.send(restaurantes);
+});
+
 app.post('/api/CadastraRestaurante', function(req, res) {
     console.log(req.body);
+    restaurantes.push(req.body);	
     res.send(req.body);
 }); 
 
